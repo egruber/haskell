f2b7afcaa5cf021ebef2b0d3679dd7fa536b0b87
Eric Gruber <lonephoenix@gmail.com>
2017-10-01 21:21:48 -0400

    Update learn.hs to remove unneeded indent

--- chapter2/learn.hs
+++ chapter2/learn.hs
@@ -1,5 +1,7 @@
 -- Created by Eric Gruber
 module Learn where
-    x = 10 * 5 + y
-    myResult = x * 5
-    y = 10
+-- Removed the indentation. It's probably going to be a thing I do later
+-- But right now, it's not helpful. And it's bad practice
+x = 10 * 5 + y
+myResult = x * 5
+y = 10
